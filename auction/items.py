91bf25362b52c70daaa133f66ebb54c97193b04f
Cai Liosatos <cai.liosatos@gmail.com>
2020-09-25 14:01:30 +1000

    removed placeholder images, added /watchlist tag in url

---
+++ auction/items.py
@@ -0,0 +1,8 @@
+#this will have the code for directing between the items listed/created (*cough cough* the destinations.py file in the tute *cough cough*)
+from flask import Blueprint, flash, render_template, request, url_for, redirect 
+import os
+from werkzeug.utils import secure_filename
+from flask_login import login_required, current_user
+
+#create a blueprint
+bp = Blueprint('destination', __name__, url_prefix='/destinations')
