f29915ed1332e34476d7c01b9dba862d3fa4039c
Cai Liosatos <cai.liosatos@gmail.com>
2020-09-26 10:44:04 +1000

    reverting

--- auction/views.py
+++ auction/views.py
@@ -6,7 +6,3 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html')
-
-@bp.route('/watchlist', methods='GET')
-def index():
-    return render_template('watchlist.html')
