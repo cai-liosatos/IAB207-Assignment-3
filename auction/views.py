bd8c92e0523d49cc82b712ba0239a905515aa38e
Cai Liosatos <cai.liosatos@gmail.com>
2020-09-26 10:39:41 +1000

    adding watchlist page

--- auction/views.py
+++ auction/views.py
@@ -6,3 +6,7 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/watchlist', methods='GET')
+def index():
+    return render_template('watchlist.html')
