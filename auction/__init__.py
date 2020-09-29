eb53b661509f13e17b21fe252363a57b8683dd42
Cai Liosatos <cai.liosatos@gmail.com>
2020-09-29 15:40:25 +1000

    database works now

--- auction/__init__.py
+++ auction/__init__.py
@@ -32,11 +32,11 @@ def create_app():
     login_manager.login_view='auth.login'
     login_manager.init_app(app)
 
-    #create a user loader function takes userid and returns User
-    #from .models import User  # importing here to avoid circular references
-    #@login_manager.user_loader
-    #def load_user(user_id):
-    #    return User.query.get(int(user_id))
+    # create a user loader function takes userid and returns User
+    from .models import User  # importing here to avoid circular references
+    @login_manager.user_loader
+    def load_user(user_id):
+       return User.query.get(int(user_id))
 
     #importing views module here to avoid circular references
     # a commonly used practice.
--- auction/models.py
+++ auction/models.py
@@ -4,34 +4,20 @@ from datetime import datetime
 
 
 
-class User(db.Model, UserMixin):
-    __tablename__='users' # good practice to specify table name
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), index=True, unique=True, nullable=False)
-    emailid = db.Column(db.String(100), index=True, nullable=False)
-	#password is never stored in the DB, an encrypted password is stored
-	# the storage should be at least 255 chars long
-    password_hash = db.Column(db.String(255), nullable=False)
-
-    # relation to call user.comments and comment.created_by
-    comments = db.relationship('Comment', backref='user')
-# class User(db.Model, UserMixin): 
-# 	__tablename__= 'users'
-# 	id = db.Column(db.Integer, primary_key=True)
-# 	username = db.Column(db.String(100), index=True, unique=True, nullable=False)
-# 	email = db.Column(db.String(100), index=True, unique=True, nullable=False)
-# 	password_hash = db.Column(db.String(255), nullable=True)
-# 	watchlist = db.relationship('Watchlist', backref='user')
-# 	bid = db.relationship('Bid', backref='user')
-
-# 	def __repr__(self):
-# 		return "<Name: {}>".format(self.username)
+class User(db.Model, UserMixin): 
+	__tablename__= 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(100), index=True, unique=True, nullable=False)
+	email = db.Column(db.String(100), index=True, unique=True, nullable=False)
+	password_hash = db.Column(db.String(255), nullable=True)
+	watchlist = db.relationship('Watchlist', backref='user')
+	bid = db.relationship('Bid', backref='user')
 
 
 class Item(db.Model):
 	__tablename__= 'items'
 	id = db.Column(db.Integer, primary_key=True)
-	Name = db.Column(db.String(100), unique=False, nullable=False, backref='watchlist')
+	name = db.Column(db.String(100), unique=False, nullable=False)
 	category = db.Column(db.String(100), index=True, unique=False, nullable=False)
 	manufacturer = db.Column(db.String(100), unique=False, nullable=True)
 	condition = db.Column(db.String(100), unique=False, nullable=True)
@@ -46,26 +32,19 @@ class Item(db.Model):
 	watchlist = db.relationship('Watchlist', backref='item')
 	bid = db.relationship('Bid', backref='item')
 
-	def __repr__(self): #string print method
-		return "<Name: {}>".format(self.name)
-
-
 
 class Bid(db.Model):
 	__tablename__= 'bid'
 	id = db.Column(db.Integer, primary_key=True)
-	userID = db.Column(db.Integer, db.Foreign('users.id'))
-	itemId = db.Column(db.Integer, db.Foreign('items.itemId'))
+	userID = db.Column(db.Integer, db.ForeignKey('users.id'))
+	itemId = db.Column(db.Integer, db.ForeignKey('items.id'))
 	amount = db.Column(db.Integer, unique=False, nullable=False)
 	time = db.Column(db.DateTime, default=datetime.now())
-
-	def __repr__(self):
-		return "<Bid: {}>".format(self.amount)
 
 class Watchlist(db.Model):
 	__tablename__= 'watchlist'
 	id = db.Column(db.Integer, primary_key=True)
 	userID = db.Column(db.Integer, db.ForeignKey('users.id'))
-	itemName = db.Column(db.String(100), db.ForeignKey('items.itemName'), unique=False)
+	itemName = db.Column(db.String(100), db.ForeignKey('items.name'), unique=False)
 	addedDate = db.Column(db.DateTime, default=datetime.now())
 
