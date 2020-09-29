1f71c410035cb27bf59196c7438ff1494433d43c
Cai Liosatos <cai.liosatos@gmail.com>
2020-09-29 14:50:52 +1000

    database shenanigans

--- auction/models.py
+++ auction/models.py
@@ -7,7 +7,7 @@ class User(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(100), index=True, unique=True, nullable=False)
 	email = db.Column(db.String(100), index=True, unique=True, nullable=False)
-	password_hash = db.Column(db.String(255), nullable=False)
+	password_hash = db.Column(db.String(255), nullable=True)
 	watchlist = db.relationship('Watchlist', backref='user')
 	bid = db.relationship('Bid', backref='user')
 
