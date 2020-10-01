3d17466c0f969c3fbe5e852a615c55dd9644cad0
Cai Liosatos <cai.liosatos@gmail.com>
2020-10-01 22:06:44 +1000

    troubleshooting

--- auction/views.py
+++ auction/views.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for
 from .models import Item
 
-mainbp = Blueprint('main', __name__)
+bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
     items = Item.query.all()
-    return render_template('index.html', items = items)
+    return render_template('index.html', items=items)
 
 @bp.route('/search')
 def search():
