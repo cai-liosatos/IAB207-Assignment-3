22d50e20848a8e93f969c24d7ca49aa42d82ab12
Cai Liosatos <cai.liosatos@gmail.com>
2020-10-03 15:49:45 +1000

    debugging /create

--- auction/items.py
+++ auction/items.py
@@ -34,8 +34,8 @@ def create():
         db.session.add(item)
         db.session.commit()
         # items = Item.query.all()
-        # return render_template('items/create.html', form=form)
-    return render_template('index.html', form=form)
+        return redirect('destinations.create')
+    return render_template('Items/create.html', form=form)
     
 
 
