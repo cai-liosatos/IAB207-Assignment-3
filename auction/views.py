aa04ce37371d065fd481f250875dda90383544fd
jnqut5 <jngem1@gmail.com>
2020-10-20 15:57:17 +1000

    add item

--- auction/views.py
+++ auction/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for
-from .models import Item
+from .models import Item, Watchlist
+from . import db
 
 bp = Blueprint('main', __name__)
 
@@ -17,3 +18,10 @@ def search():
         return render_template('index.html', items=items)
     else:
         return redirect(url_for('main.index'))
+
+@bp.route('/add')
+def add_to_watchlist():
+    watchlist_item = Watchlist()
+    db.session.add(watchlist_item)
+    db.session.commit()
+    return redirect(url_for('index'))
