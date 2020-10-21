a410f6c38fa38e0310597dd57eb057e6f6e0d62d
jnqut5 <jngem1@gmail.com>
2020-10-21 23:05:59 +1000

    adding (/add and /watchlist) into watchlist.py

--- auction/watchlist.py
+++ auction/watchlist.py
@@ -8,6 +8,20 @@ from flask_login import login_required, current_user
 bp = Blueprint('watchlist', __name__)
 from flask import session
 
+@bp.route('/add')
+def add_to_watchlist():
+    watchlist_item = Watchlist()
+    db.session.add(watchlist_item)
+    db.session.commit()
+    return redirect(url_for('main.index'))
+
+@bp.route('/watchlist', methods=['GET', 'POST'])
+def watchlist():
+    watchlist = Watchlist.query.filter_by(id=id).first()
+    db.session.add(item)
+    db.session.commit()
+    return render_template('watchlist.html', watchlist=watchlist)
+
 @bp.route('/watchlist', methods=['GET', 'POST'])
 def watchlist():
     watchlist = Watchlist.query.filter_by(id=id).first()
