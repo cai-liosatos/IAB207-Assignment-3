455beebfab2c95bc1ada2a7c655f5667c450d222
jnqut5 <jngem1@gmail.com>
2020-10-21 23:02:52 +1000

    removing (/add) from views

--- auction/views.py
+++ auction/views.py
@@ -19,10 +19,5 @@ def search():
     else:
         return redirect(url_for('main.index'))
 
-@bp.route('/add')
-def add_to_watchlist():
-    watchlist_item = Watchlist()
-    db.session.add(watchlist_item)
-    db.session.commit()
-    return redirect(url_for('main.index'))
-    
+
+
