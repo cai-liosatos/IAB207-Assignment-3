df688a0e455af931de38d51ef0a938025c28713a
Cai Liosatos <cai.liosatos@gmail.com>
2020-11-01 22:16:37 +1000

    wtf weir dbug

--- auction/items.py
+++ auction/items.py
@@ -28,15 +28,15 @@ def check_upload_file(form):
 @bp.route('/<id>') 
 @login_required
 def show(id):
-  item = Item.query.filter_by(id=id).first()
-  similar_items = Item.query.filter_by(category=item.category).order_by(func.random()).limit(4)
-  if current_user.id == item.userID:
-    bidList1 = Bid.query.filter_by(itemId=item.id).order_by(desc(Bid.amount))
-    user_details = User.query.filter(and_(User.id == Bid.userID, Bid.itemId == item.id))
-    bidList2 = zip(bidList1, user_details)
-    return render_template('items/show.html', similar_items=similar_items, item=item, bidList=bidList2)
-  else:
-    return render_template('items/show.html', similar_items=similar_items, item=item)
+    item = Item.query.filter_by(id=id).first()
+    similar_items = Item.query.filter_by(category=item.category).order_by(func.random()).limit(4)
+    if current_user.id == item.userID:
+        bidList1 = Bid.query.filter_by(itemId=item.id).order_by(desc(Bid.amount))
+        user_details = User.query.filter(and_(User.id == Bid.userID, Bid.itemId == item.id))
+        bidList2 = zip(bidList1, user_details)
+        return render_template('items/show.html', similar_items=similar_items, item=item, bidList=bidList2)
+    else:
+        return render_template('items/show.html', similar_items=similar_items, item=item)
 
 # function to create an item, pulled info from form and adds a new row to the Items table
 @bp.route('/create', methods = ['GET', 'POST'])
@@ -48,7 +48,7 @@ def create():
         db_file_path=check_upload_file(form)
         item=Item(name=form.name.data, category=form.category.data, manufacturer=form.manufacturer.data, condition=form.condition.data, image=db_file_path, 
         finishDate=form.finishdate.data, deliveryTime=form.postagedate.data, currentPrice=form.startingprice.data, postagePrice=form.postageprice.data, 
-        currency=form.currency.data, moreInfo=form.description.data, status="open")
+        currency=form.currency.data, moreInfo=form.description.data, status="open", userID=current_user.id)
         db.session.add(item)
         db.session.commit()
         return redirect(url_for('item.show', id=item.id))
@@ -94,4 +94,3 @@ def close(id):
     else:
         flash('Sorry, this auction is already closed', 'warning')
         return redirect(url_for('main.index'))
-
